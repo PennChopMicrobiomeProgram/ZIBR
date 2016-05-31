2c4daebb5f6baa48d8fb0e77ca8a6f789cdbb8ea
Eric Z Chen <chvlyl@gmail.com>
2016-05-31 14:08:48 -0400

    add real data

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Longitudinal human microbiome data
+#'
+#' A dataset containing the bacterial abundance and clinical information from
+#' a longitudinal human microbiome study
+#'
+#' @docType data
+#' @usage data(ibd.data)
+#' @keywords datasets
+#'
+#' @format A data frame with 236 rows and 5 variables:
+#' \describe{
+#'   \item{Sample}{Sample IDs}
+#'   \item{Subject}{Subject IDs}
+#'   \item{Time}{Time points}
+#'   \item{Treatment}{Treatment, 0 for antiTNF, 1 for EEN}
+#'   \item{Abundance}{Abundance for Eubacterium}
+#'   ...
+#' }
+#' @references Lewis and Chen et al. (2016) Cell Host & Microbe 18 (4), 489-500
+"ibd.data"
