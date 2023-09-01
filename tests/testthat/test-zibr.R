3cfa19a698f7e4ef9b34c09f1afc9c69cc70b065
ctbus <ctbushman@gmail.com>
2023-08-31 22:27:18 -0400

    Update tests to use new data obj name

--- tests/testthat/test-zibr.R
+++ tests/testthat/test-zibr.R
@@ -1,10 +1,10 @@
 E <- list(logistic.est.table = structure(c(2.69732340911363, 0.200018071642455, 6.17105397782147e-06, 0.887710710500198), .Dim = c(2L, 2L), .Dimnames = list(c("intersept", "Treatment"), c("Estimate", "Pvalue"))), logistic.s1.est = 3.27876357265477, beta.est.table = structure(c(-2.78605402671509, -0.32334269957996, 0, 0.206300170582503), .Dim = c(2L, 2L), .Dimnames = list(c("intersept", "Treatment"), c("Estimate", "Pvalue"))), beta.s2.est = 0.538923350735293, beta.v.est = 7.62238420518043, loglikelihood = 321.901751058239, joint.p = c(Treatment = 0.445494681803514))
 
 test_that("zibr main function works", {
-  zibr.fit <- zibr(logistic.cov = data.frame(Treatment=ibd.data$Treatment),
-                   beta.cov = data.frame(Treatment=ibd.data$Treatment),
-                   Y = ibd.data$Abundance, subject.ind = ibd.data$Subject,
-                   time.ind = ibd.data$Time)
+  zibr.fit <- zibr(logistic.cov = data.frame(Treatment=ibd$Treatment),
+                   beta.cov = data.frame(Treatment=ibd$Treatment),
+                   Y = ibd$Abundance, subject.ind = ibd$Subject,
+                   time.ind = ibd$Time)
   print("HEYS")
   dput(zibr.fit)
 
