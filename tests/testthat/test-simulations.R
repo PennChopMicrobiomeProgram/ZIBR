96c418c0a504cc0593bd3d36b342bce61f08cf52
Kyle Bittinger <kylebittinger@gmail.com>
2026-03-06 08:27:07 -0500

    Remove betareg fits from tests

--- tests/testthat/test-simulations.R
+++ tests/testthat/test-simulations.R
@@ -14,16 +14,6 @@ expected_bre_fit <- list(
 
 test_that("simulate_beta_data works", {
   beta_data <- simulate_beta_random_effect_data()
-
-  # betareg can not fit random effect model so set the s2 to a small value (small random effect)
-  tdata <- data.frame(Y = beta_data$Y, beta_data$Z, SID = beta_data$subject_ind)
-  gy <- betareg::betareg(beta_data$Y ~ log.Time + as.factor(Treatment), data = tdata, type = "ML")
-  #print(summary(gy))
-  gy <- betareg::betareg(beta_data$Y ~ log.Time + as.factor(Treatment), data = tdata, type = "BC")
-  #print(summary(gy))
-  gy <- betareg::betareg(beta_data$Y ~ log.Time + as.factor(Treatment), data = tdata, type = "BR")
-  #print(summary(gy))
-
   res <- fit_beta_random_effect(Z = beta_data$Z,
                                 Y = beta_data$Y,
                                 subject.ind = beta_data$subject_ind,
