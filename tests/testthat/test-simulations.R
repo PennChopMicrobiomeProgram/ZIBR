dd8a824854325676ca5781cede40a3e3660dd772
Kyle Bittinger <kylebittinger@gmail.com>
2026-03-06 08:41:52 -0500

    Reformat test code with styler

--- tests/testthat/test-simulations.R
+++ tests/testthat/test-simulations.R
@@ -1,7 +1,9 @@
 expected_bre_fit <- list(
   est.table = matrix(
-    c(-0.60955243747617, -0.443501946207444, 0.736943004541776,
-      0.0104576873063189, 0.000414042620671129, 0.0123799088684966),
+    c(
+      -0.60955243747617, -0.443501946207444, 0.736943004541776,
+      0.0104576873063189, 0.000414042620671129, 0.0123799088684966
+    ),
     nrow = 3,
     dimnames = list(
       c("intersept", "log.Time", "Treatment"),
@@ -14,20 +16,24 @@ expected_bre_fit <- list(
 
 test_that("simulate_beta_data works", {
   beta_data <- simulate_beta_random_effect_data()
-  res <- fit_beta_random_effect(Z = beta_data$Z,
-                                Y = beta_data$Y,
-                                subject.ind = beta_data$subject_ind,
-                                time.ind = beta_data$time_ind,
-                                quad.n = 30,
-                                verbose = FALSE)
+  res <- fit_beta_random_effect(
+    Z = beta_data$Z,
+    Y = beta_data$Y,
+    subject.ind = beta_data$subject_ind,
+    time.ind = beta_data$time_ind,
+    quad.n = 30,
+    verbose = FALSE
+  )
 
   expect_equal(res, expected_bre_fit, tolerance = 1e-3)
 })
 
 expected_lre_fit <- list(
   est.table = matrix(
-    c(0.306795205483351, 0.280454254105502,
-      -0.667159092633291, 0.431194361789462, 0.266160176079596, 0.123214729652536),
+    c(
+      0.306795205483351, 0.280454254105502,
+      -0.667159092633291, 0.431194361789462, 0.266160176079596, 0.123214729652536
+    ),
     nrow = 3,
     dimnames = list(c("intersept", "log.Time", "Treatment"), c("Estimate", "Pvalue"))
   ),
@@ -37,17 +43,23 @@ expected_lre_fit <- list(
 test_that("simulate_logistic_data works", {
   logistic_data <- simulate_logistic_data()
 
-  tdata <- data.frame(Y = logistic_data$Y, logistic_data$X, SID = logistic_data$subject_ind)
-  lme.fit <- lme4::glmer(as.factor(logistic_data$Y) ~ log.Time + as.factor(Treatment) + (1 | SID),
-                         data = tdata,
-                         family = binomial,
-                         control = lme4::glmerControl(optimizer = "bobyqa"),
-                         nAGQ = 10)
+  tdata <- data.frame(
+    Y = logistic_data$Y, logistic_data$X, SID = logistic_data$subject_ind
+  )
+  lme.fit <- lme4::glmer(
+    as.factor(logistic_data$Y) ~ log.Time + as.factor(Treatment) + (1 | SID),
+    data = tdata,
+    family = binomial,
+    control = lme4::glmerControl(optimizer = "bobyqa"),
+    nAGQ = 10
+  )
   summary(lme.fit)
 
-  res <- fit_logistic_random_effect(X = logistic_data$X,
-                                    Y = logistic_data$Y,
-                                    subject.ind = logistic_data$subject_ind,
-                                    time.ind = logistic_data$time_ind)
+  res <- fit_logistic_random_effect(
+    X = logistic_data$X,
+    Y = logistic_data$Y,
+    subject.ind = logistic_data$subject_ind,
+    time.ind = logistic_data$time_ind
+  )
   expect_equal(res, expected_lre_fit, tolerance = 1e-3)
 })
